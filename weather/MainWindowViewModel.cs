2a3478e4463ac04974da699e58eb6630bff7452b
YaLizka <ya.liz.ka@yandex.ru>
2017-12-17 12:02:52 +0300

    Signed-off-by: YaLizka <ya.liz.ka@yandex.ru>

---
+++ weather/MainWindowViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HowIsTheWeather
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                _city = value;
+                OnPropertyChanged("City");
+            }
+        }
+        private string _location;
+        public string Location
+        {
+            get { return _location; }
+            set
+            {
+                _location = value;
+                OnPropertyChanged("Location");
+            }
+        }
+        private string _temperature;
+        public string Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                OnPropertyChanged("Temperature");
+            }
+        }
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged("Status");
+            }
+        }
+        private string _wind;
+        public string Wind
+        {
+            get { return _wind; }
+            set
+            {
+                _wind = value;
+                OnPropertyChanged("Wind");
+            }
+        }
+        private string _humidity;
+        public string Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                _humidity = value;
+                OnPropertyChanged("Humidity");
+            }
+        }
+        private string _pressure;
+        public string Pressure
+        {
+            get { return _pressure; }
+            set
+            {
+                _pressure = value;
+                OnPropertyChanged("Pressure");
+            }
+        }
+        private string _search;
+        public string Search
+        {
+            get { return _search; }
+            set
+            {
+                _search = value;
+                OnPropertyChanged("Search");
+            }
+        }
+        public Command SearchCommand { get; set; } = new Command();
+        public MainWindowViewModel()
+        {
+            SearchCommand.Func = DoSearch;
+        }
+        private void DoSearch(object parameter)
+        {
+            WeatherAPI api = new WeatherAPI();
+            Weather w = api.GetWeather(Search);
+            if (w == null) return;
+            if (w.Query.Count == 0)
+            {
+                MessageBox.Show("Нет результата по запросу");
+                return;
+            }
+            City = w.Query.Results.Channel.Location.City;
+            Location = $"{w.Query.Results.Channel.Location.Region.Trim()},\n{w.Query.Results.Channel.Location.Country.Trim()}";
+            Temperature = $"{w.Query.Results.Channel.Item.Condition.Temp} {w.Query.Results.Channel.Units.Temperature}";
+            Status = w.Query.Results.Channel.Item.Condition.Text;
+            Wind = $"{w.Query.Results.Channel.Wind.Speed} {w.Query.Results.Channel.Units.Speed}";
+            Humidity = w.Query.Results.Channel.Atmosphere.Humidity;
+            Pressure = $"{w.Query.Results.Channel.Atmosphere.Pressure} {w.Query.Results.Channel.Units.Pressure}";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public Action<object> Func { get; set; }
+        public void Execute(object parameter)
+        {
+            Func(parameter);
+        }
+    }
+}
---
+++ weather/Weather.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HowIsTheWeather
+{
+    class WeatherAPI
+    {
+        public Weather GetWeather(string city)
+        {
+            try
+            {
+                WebRequest r = WebRequest.Create($"https://query.yahooapis.com/v1/public/yql?format=json&q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"{city}\") and u=\'c\'");
+                WebResponse w = r.GetResponse();
+                string a = string.Empty;
+                using (StreamReader c = new StreamReader(w.GetResponseStream()))
+                    a = c.ReadToEnd();
+                return JsonConvert.DeserializeObject<Weather>(a);
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно загрузить погоду");
+                return null;
+            }
+        }
+    }
+    class Weather
+    {
+        public Query Query { get; set; }
+    }
+    class Query
+    {
+        public int Count { get; set; }
+        public Results Results { get; set; }
+    }
+    class Results
+    {
+        public Channel Channel { get; set; }
+    }
+    class Channel
+    {
+        public Units Units { get; set; }
+        public Location Location { get; set; }
+        public Wind Wind { get; set; }
+        public Item Item { get; set; }
+        public Atmosphere Atmosphere { get; set; }
+    }
+    class Atmosphere
+    {
+        public string Humidity { get; set; }
+        public string Pressure { get; set; }
+    }
+    class Units
+    {
+        public string Distance { get; set; }
+        public string Pressure { get; set; }
+        public string Speed { get; set; }
+        public string Temperature { get; set; }
+    }
+    class Location
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+    }
+    class Wind
+    {
+        public float Speed { get; set; }
+    }
+    class Item
+    {
+        [JsonProperty(PropertyName = "condition")]
+        public Condition Condition { get; set; }
+        public List<Condition> Forecast { get; set; }
+    }
+    class Condition
+    {
+        public string Date { get; set; }
+        public string Day { get; set; }
+        public int High { get; set; }
+        public int Low { get; set; }
+        public int Temp { get; set; }
+        public string Text { get; set; }
+    }
+}
